6848f48a19c0394a3c9477e3a6b4c6e00ea629c1
David Ferris <david.j.ferris.21@gmail.com>
2017-06-07 15:42:40 -0700

    refactoring

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from extractors import yahoo
 
 app = Flask(__name__)
+if __name__ == "__main__":
+	app.run()
 
 @app.route('/')
 def index():
 	return '<h1>Bobit</h1>'
 
-if __name__ == "__main__":
-	app.run()
+@app.route('/yahoo')
+def yahoo_test():
+	return yahoo.getData('YHOO', '2014-04-25', '2014-04-29')
